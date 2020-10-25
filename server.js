d87048755e8deb2511c1e31e44abd9ddd7374740
Nishal Kulkarni <kulknishu@gmail.com>
2020-10-26 01:07:56 +0530

    Added devices, apps and feedback features

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.get('/logout', (req, res) => {
 
 var dashboardFunction = () => {
     return new Promise((resolve, reject) => {
-        var sql = "SELECT app_name,description,cost,DATE_FORMAT(last_updated, '%d-%M-%y') last_updated\
+        var sql = "SELECT app_id,app_name,description,cost,DATE_FORMAT(last_updated, '%d-%M-%y') last_updated\
          FROM app;"
         db.query(sql, async (error, results) => {
             if (results.length == 0) {
@@ -180,11 +180,11 @@ var dashboardFunction = () => {
 app.get('/dashboard', (req, res) => {
     if (req.session.user && req.cookies.user_sid) {
         dashboardFunction()
-        .then((results) => {
-            return res.render('dashboard', { results })
-        }).catch((error) => {
-            return res.render('dashboard', { error })
-        })
+            .then((results) => {
+                return res.render('dashboard', { results })
+            }).catch((error) => {
+                return res.render('dashboard', { error })
+            })
     } else {
         res.redirect('/login');
     }
@@ -214,13 +214,143 @@ var rankingsFunction = () => {
 app.get('/rankings', (req, res) => {
     rankingsFunction()
         .then((results) => {
-            console.log(results[0].app_id);
-            console.log(results);
             return res.render('rankings', { results })
         }).catch((error) => {
             return res.render('rankings', { error })
         })
 });
+
+var devicesFunction = (uid) => {
+    return new Promise((resolve, reject) => {
+        var sql = "SELECT device_id,os \
+         FROM devices WHERE user_id = ?";
+        db.query(sql, [uid], async (error, results) => {
+            if (results.length == 0) {
+                reject('No devices found. Please add a device');
+            } else {
+                resolve(results);
+            }
+        })
+    });
+}
+
+var addDevice = (uid, os) => {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT device_id FROM devices;', async (error, results) => {
+            if (error) console.log(error);
+            else {
+                var dids = [];
+                results.forEach(element => {
+                    dids.push(element.device_id);
+                });
+
+                let i = 10001;
+                while (dids.includes('D' + i)) {
+                    i += 1;
+                }
+                let newdid = 'D' + i;
+                let params = { device_id: newdid, user_id: uid, os: os };
+                db.query('INSERT INTO devices SET ? ', params, (error, re) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        resolve(params)
+                    }
+                });
+            }
+        });
+    });
+}
+
+app.route('/devices')
+    .get((req, res) => {
+        var uid = req.session.user.user_id;
+        devicesFunction(uid)
+            .then((results) => {
+                console.log(results)
+                return res.render('devices', { results })
+            }).catch((error) => {
+                return res.render('devices', { error })
+            })
+    }).post((req, res) => {
+        try {
+            var os = req.body.os;
+            var uid = req.session.user.user_id;
+
+            addDevice(uid, os)
+                .then((results) => {
+                    res.redirect('/devices')
+                }).catch((error) => {
+                    res.redirect('/devices')
+                })
+
+
+        } catch (error) {
+            console.log(error)
+        }
+    });
+
+
+var getAppInfo = (aid) => {
+    return new Promise((resolve, reject) => {
+        var sql = "SELECT * \
+             FROM app NATURAL JOIN developer NATURAL JOIN supported_platforms WHERE app_id = ?";
+        db.query(sql, [aid], async (error, results) => {
+            if (results.length == 0) {
+                reject('App not found');
+            } else {
+                resolve(results);
+            }
+        })
+    });
+}
+
+var sendFeedback = (aid, uid, rating, review) => {
+    return new Promise((resolve, reject) => {
+        let params = { user_id: uid, app_id: aid, rating: rating, review: review };
+        db.query('INSERT INTO feedback SET ? ', params, (error, re) => {
+            if (error) {
+                console.log(error);
+            } else {
+                resolve(params)
+            }
+        });
+    });
+}
+
+app.route('/app/app*')
+    .get((req, res) => {
+        var aid = req.url.substring(5)
+        console.log(aid)
+        // res.render('app');
+        getAppInfo(aid)
+            .then((results) => {
+                console.log(results)
+                return res.render('app', { results })
+            }).catch((error) => {
+                return res.render('app', { error })
+            })
+    }).post((req, res) => {
+        var aid = req.url.substring(5);
+        var uid = req.session.user.user_id;
+        var rating = req.body.rating;
+        var review = req.body.review;
+
+        sendFeedback(aid, uid, rating, review)
+            .then((results) => {
+                getAppInfo(aid)
+                    .then((rei) => {
+                        console.log(rei)
+                        return res.render('app', { message: "Feedback sent successfully!" })
+                    }).catch((error) => {
+                        return res.render('app', { error })
+                    })
+            }).catch((error) => {
+                return res.render('app', { error })
+            })
+
+    });
+
 
 app.use(function (req, res, next) {
     res.status(404).send("Sorry can't find that!")
@@ -236,4 +366,9 @@ db.connect(function (err) {
     });
 });
 
+
+
 app.listen(2678, () => console.log('Server started at http://localhost:2678'))
+
+// pmallia2@mysql.com
+// FRqEM9R1
